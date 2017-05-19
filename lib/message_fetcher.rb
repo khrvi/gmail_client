a4f405e1ff38601b92e50e57a1bde275830674a8
Viacheslav Khrenov <slava@rightscale.com>
2017-05-19 20:26:22 +0300

    save only uniq messages

--- lib/message_fetcher.rb
+++ lib/message_fetcher.rb
@@ -9,13 +9,15 @@ class MessageFetcher
     Gmail.connect(ENV['GMAIL_USER_NAME'], ENV['GMAIL_USER_PASSWORD']) do |gmail|
       if gmail.logged_in?
         gmail.inbox.emails(:all).each do |email|
+          next if Message.find_by_message_id(email.msg_id)
           Message.create(
             message_id: email.msg_id,
             body: email.message.body,
             label: email.labels.to_s,
             to: email.message.to,
             from: email.message.from,
-            subject: email.message.subject)
+            subject: email.message.subject
+          )
         end
       else
         puts "Login failed."
