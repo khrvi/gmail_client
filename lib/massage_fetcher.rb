b616b9317e1ac7fc56bb48726c885c56c5c3a16d
Viacheslav Khrenov <slava@rightscale.com>
2017-05-19 19:21:07 +0300

    use sidekiq for pulling messages

---
+++ lib/massage_fetcher.rb
@@ -0,0 +1,20 @@
+require 'gmail'
+require 'sidekiq-scheduler'
+
+class MessageFetcher
+  include Sidekiq::Worker
+
+  def perform
+    # set creds through env variables
+    ::Gmail.connect(ENV['GMAIL_USER_NAME'], ENV['GMAIL_USER_PASSWORD']) do |gmail|
+      if gmail.logged_in?
+        gmail.inbox.emails(:all).each do |email|
+          # put a couple attrs to DB for now
+          Message.create(message_id: email.msg_id, body: email.message, label: email.labels)
+        end
+      else
+        puts "Login failed."
+      end
+    end
+  end
+end
